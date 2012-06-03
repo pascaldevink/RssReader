e48b58157075c035b5c9c62ac56799354d9a4d00
Pascal de Vink <pascal.de.vink@gmail.com>
2012-06-03 20:12:25 +0200

    Added the readability bundle to the config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,11 +22,11 @@ class AppKernel extends Kernel
             new Pascal\FeedDisplayerBundle\PascalFeedDisplayerBundle(),
             new Pascal\SimplePieRssBundle\PascalSimplePieRssBundle(),
             new Pascal\TwitterOAuthBundle\PascalTwitterOAuthBundle(),
+            new Pascal\ReadabilityBundle\PascalReadabilityBundle(),
 		);
 
 		if (in_array($this->getEnvironment(), array('dev', 'test')))
 		{
-//			$bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
 			$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
 			$bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 			$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
